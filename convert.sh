39ac3a7c1e2edf3fee18487a4756c9774e6483db
Filip Sobalski <pinkeen@gmail.com>
2015-04-22 00:34:27 +0200

    ...

--- convert.sh
+++ convert.sh
@@ -1,11 +1,43 @@
 #!/usr/bin/env bash
 
-find . -iname '*.cg' | while read src ; do
-  dst="${src%.cg}.glsl"
-  echo "Converting $src --> $dst"
+function convert() {
+  
+    src="$1"
+    rel="${1#$srcdir}"
+    pth="${dstdir}${rel}"
+    dst="${pth%$2}$3";
+    
+    mkdir -p "$(dirname $dst)"
+    
+    echo "Converting $src --> $dst" | tee -a $logfile
+    cg2glsl "$src" "$dst" 2>&1 >> $logfile
+    
+    if [ $? -eq 0 ]; then
+        echo -e "\tOK"
+    else
+        echo -e "\tFAIL"
+        echo "$dst" >> $failfile
+    fi
+}
+
+srcdir="cg/"
+dstdir="glsl/"
+
+logfile="convert.log"
+failfile="${dstdir}/failed.log"
+
+cd cg 
+git pull
+echo `git log --pretty=format:"[%cd] %h %s" -1` > "../${dstdir}/REV"
+cd ..
+
+echo '' > $logfile
+echo '' > $failfile
+
+find "$srcdir" -iname '*.cg' | while read src ; do
+  convert "$src" cg glsl
 done
 
-find . -iname '*.cgp' | while read src ; do
-  dst="${src%.cgp}.glslp"
-  echo "$src --> $dst"
+find "$srcdir" -iname '*.cgp' | while read src ; do
+  convert "$src" cgp glslp
 done
